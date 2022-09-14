8a30853f23047ebe8d835b406ba584dd58d2222b
Wanny Martins <wcatery@gmail.com>
2022-09-14 19:47:28 -0300

    feat(iniciando uma rota para cars com um service apenas com create):

---
+++ src/services/CarService.ts
@@ -0,0 +1,17 @@
+import { carZodSchema, ICar } from '../interfaces/ICar';
+import { IModel } from '../interfaces/IModel';
+
+export default class CarService implements IModel<ICar> {
+  private _car: IModel<ICar>;
+
+  constructor(model: IModel<ICar>) {
+    this._car = model;
+  }
+
+  public async create(obj: ICar): Promise<ICar> {
+    const parsed = carZodSchema.safeParse(obj);
+    if (!parsed.success) throw parsed.error;
+
+    return this._car.create(obj);
+  }
+}
