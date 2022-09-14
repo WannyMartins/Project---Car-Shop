45d91f790554bd4fa712df24563fd877091b1aad
Wanny Martins <wcatery@gmail.com>
2022-09-14 19:46:53 -0300

    feat(Model):

---
+++ src/models/CarModel.ts
@@ -0,0 +1,18 @@
+import { model as mongooseCreateModel, Schema } from 'mongoose';
+import { ICar } from '../interfaces/ICar';
+import MongoModel from './MongoModel';
+
+const carMongooseSchema = new Schema<ICar>({
+  status: Boolean,
+  model: String,
+  year: Number,
+  color: String,
+  buyValue: Number,
+  doorsQty: Number,
+  seatsQty: Number });
+
+export default class CarModel extends MongoModel<ICar> {
+  constructor(model = mongooseCreateModel('CarModel', carMongooseSchema)) {
+    super(model);
+  }
+}
