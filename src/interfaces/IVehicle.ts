5d6c0363b4fba04ca5a72a55cbbb019111031913
Wanny Martins <wcatery@gmail.com>
2022-09-14 18:40:46 -0300

    refactor(interface): refatorando IVehicle para uso do zod

--- src/interfaces/IVehicle.ts
+++ src/interfaces/IVehicle.ts
@@ -1,7 +1,23 @@
-export interface IVehicle {
-  model: string,
-  year: number,
-  color: string,
-  status?: boolean,
-  buyValue: number 
-}
+import { z } from 'zod';
+
+const vehicleZodSchema = z.object({
+  model: z.string().min(3),
+  year: z.number().int().positive().gte(1900)
+    .lte(2022),
+  color: z.string().min(3),
+  status: z.boolean().optional(),
+  buyValue: z.number().int().positive(),
+});
+
+type IVehicle = z.infer<typeof vehicleZodSchema>;
+
+// export IVehicle;
+export { vehicleZodSchema, IVehicle };
+
+// export interface IVehicle {
+//   model: string,
+//   year: number,
+//   color: string,
+//   status?: boolean,
+//   buyValue: number 
+// }
